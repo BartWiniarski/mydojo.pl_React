db3851a8d332507fb4c4ae9c11ad16549c53c9d2
BartWiniarski <bartosz.winiarski@poczta.fm>
2023-12-11 15:49:23 +0100

    initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import GoogleFontLoader from "react-google-font-loader";
 import {AuthProvider} from "./context/AuthProvider.tsx";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <GoogleFontLoader fonts={[{font: 'Poppins', weights: [400, 700]}]}/>
-        <AuthProvider>
-            <App/>
-        </AuthProvider>
+        <BrowserRouter>
+            <AuthProvider>
+                <Routes>
+
+                </Routes>
+                <App/>
+            </AuthProvider>
+        </BrowserRouter>
     </React.StrictMode>,
 )
