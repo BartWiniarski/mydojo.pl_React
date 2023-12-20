592620c6fbd325d8faec4b50a15f4f96d5f0af02
BartWiniarski <bartosz.winiarski@poczta.fm>
2023-12-20 13:12:23 +0100

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ if (process.env.NODE_ENV === 'production') {
     disableReactDevTools();
 }
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
